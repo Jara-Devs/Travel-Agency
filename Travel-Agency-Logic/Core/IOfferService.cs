861dd39cb34e97c4e185720dadc412e7c329ee51
alexsierra45 <alexsierraalcala@gmail.com>
2023-11-04 13:50:31 -0400

    offer service

---
+++ Travel-Agency-Logic/Core/IOfferService.cs
@@ -0,0 +1,14 @@
+using Travel_Agency_Core;
+using Travel_Agency_Domain.Offers;
+using Travel_Agency_Domain.Services;
+using Travel_Agency_Logic.Request;
+using Travel_Agency_Logic.Response;
+
+namespace Travel_Agency_Logic.Core;
+
+public interface IOfferService<T> where T : Offer
+{
+    Task<ApiResponse<IdResponse>> CreateOffer(OfferRequest<T> offer, UserBasic user);
+    Task<ApiResponse> UpdateOffer(int id, OfferRequest<T> offer, UserBasic user);
+    Task<ApiResponse> DeleteOffer(int id, UserBasic user);
+}
---
+++ Travel-Agency-Logic/Services/Offer.cs
@@ -0,0 +1,83 @@
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+using Travel_Agency_Core;
+using Travel_Agency_DataBase;
+using Travel_Agency_Domain.Offers;
+using Travel_Agency_Domain.Services;
+using Travel_Agency_Logic.Core;
+using Travel_Agency_Logic.Request;
+using Travel_Agency_Logic.Response;
+
+namespace Travel_Agency_Logic.Services
+{
+    public class OfferService<T> : IOfferService<T> where T : Offer
+    {
+        private readonly TravelAgencyContext _context;
+
+        public OfferService(TravelAgencyContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ApiResponse<IdResponse>> CreateOffer(OfferRequest<T> offer, UserBasic user)
+        {
+            if (!CheckPermissions(user))
+                return new Unauthorized<IdResponse>("You don't have permissions");
+
+            if (await _context.Set<T>().AnyAsync(o => o.Name == offer.Name))
+                return new BadRequest<IdResponse>("The offer already exists");
+
+            if (!CheckValidity(offer))
+                return new BadRequest<IdResponse>("The offer is not valid");
+
+            _context.Set<T>().Add(offer.Offer());
+            await _context.SaveChangesAsync();
+
+            return new ApiResponse<IdResponse>((await this._context.Set<T>()
+                .Where(o => o.Name == offer.Name).Select(o => new IdResponse { Id = o.Id })
+                .SingleOrDefaultAsync())!);
+        }
+        
+        public async Task<ApiResponse> UpdateOffer(int id, OfferRequest<T> offer, UserBasic user)
+        {
+            if (!CheckPermissions(user))
+                return new Unauthorized("You don't have permissions");
+
+            if (!await _context.Set<T>().AnyAsync(o => o.Id == id))
+                return new NotFound("Offer not found");
+
+            if (!CheckValidity(offer))
+                return new BadRequest("The offer is not valid");
+
+            var newOffer = offer.Offer();
+            newOffer.Id = id;
+
+            _context.Update(newOffer);
+            await _context.SaveChangesAsync();
+
+            return new ApiResponse();
+        }
+
+        public async Task<ApiResponse> DeleteOffer(int id, UserBasic user)
+        {
+            if (!CheckPermissions(user))
+                return new Unauthorized("You don't have permissions");
+
+            var offer = await _context.Set<T>().FindAsync(id);
+
+            if (offer is null)
+                return new NotFound("Offer not found");
+
+            _context.Set<T>().Remove(offer);
+            await _context.SaveChangesAsync();
+
+            return new ApiResponse();
+        }
+
+        private static bool CheckPermissions(UserBasic user) =>
+            user.Role == Roles.AdminAgency || user.Role == Roles.ManagerAgency;
+
+        private static bool CheckValidity(OfferRequest<T> offer) =>
+            offer.Availability >= 0 && offer.Price >= 0 && offer.StartDate <= offer.EndDate;
+    }
+}
