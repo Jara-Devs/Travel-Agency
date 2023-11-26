b140b2d99fccf26203b4cc140b205df59c7c3627
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-11-26 15:16:56 -0500

    fix package

--- Travel-Agency-Domain/Packages/Package.cs
+++ Travel-Agency-Domain/Packages/Package.cs
@@ -7,23 +7,20 @@ namespace Travel_Agency_Domain.Packages;
 
 public class Package : Entity
 {
-    public ReactionState ReactionState { get; set; }
-
     public double Price() => this.Offers.Sum(o => o.Price) * (100 - this.Discount) / 100;
 
     public long StartDate() => this.Offers.Min(o => o.StartDate);
 
     public long EndDate() => this.Offers.Max(o => o.EndDate);
-    
+
     public double Discount { get; set; }
 
     public string Description { get; set; }
 
     public ICollection<Offer> Offers { get; set; } = null!;
 
-    public Package(ReactionState reactionState, string description, double discount = 0)
+    public Package(string description, double discount = 0)
     {
-        this.ReactionState = reactionState;
         this.Description = description;
         this.Discount = discount;
     }
--- Travel-Agency-Logic/Request/PackageRequest.cs
+++ Travel-Agency-Logic/Request/PackageRequest.cs
@@ -1,12 +1,9 @@
-using Travel_Agency_Core.Enums;
 using Travel_Agency_Domain.Packages;
 
 namespace Travel_Agency_Logic.Request;
 
 public class PackageRequest
 {
-    public ReactionState ReactionState { get; set; }
-
     public double Discount { get; set; }
 
     public string Description { get; set; } = null!;
@@ -15,10 +12,10 @@ public class PackageRequest
 
     public Package Package(Package? package = null)
     {
-        package ??= new Package(this.ReactionState, this.Description, this.Discount);
+        package ??= new Package(this.Description, this.Discount);
         package.Description = this.Description;
         package.Discount = this.Discount;
 
         return package;
-    } 
+    }
 }
