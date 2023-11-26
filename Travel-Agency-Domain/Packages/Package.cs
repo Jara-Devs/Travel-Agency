1a90b1433ff480665a4b0d8a0ab6565cea40dfb5
alexsierra45 <alexsierraalcala@gmail.com>
2023-11-26 18:51:19 -0500

    package domain

--- Travel-Agency-Domain/Packages/Package.cs
+++ Travel-Agency-Domain/Packages/Package.cs
@@ -1,5 +1,4 @@
 using Travel_Agency_Core;
-using Travel_Agency_Core.Enums;
 using Travel_Agency_Domain.Offers;
 using Travel_Agency_Domain.Payments;
 
@@ -7,17 +6,44 @@ namespace Travel_Agency_Domain.Packages;
 
 public class Package : Entity
 {
-    public double Price() => this.Offers.Sum(o => o.Price) * (100 - this.Discount) / 100;
+    public double Price() {
+        long hotel = this.HotelOffers.Sum(h => h.EndDate);
+        long excursion = this.ExcursionOffers.Sum(e => e.EndDate);
+        long flight = this.FlightOffers.Sum(f => f.EndDate);
 
-    public long StartDate() => this.Offers.Min(o => o.StartDate);
+        var values = new long[] {hotel, excursion, flight};
 
-    public long EndDate() => this.Offers.Max(o => o.EndDate);
+        return values.Sum() * (100 - this.Discount) / 100;
+    } 
 
+    public long StartDate() {
+        long hotel = this.HotelOffers.Min(h => h.EndDate);
+        long excursion = this.ExcursionOffers.Min(e => e.EndDate);
+        long flight = this.FlightOffers.Min(f => f.EndDate);
+
+        var values = new long[] {hotel, excursion, flight};
+
+        return values.Min();
+    }
+
+    public long EndDate() {
+        long hotel = this.HotelOffers.Max(h => h.EndDate);
+        long excursion = this.ExcursionOffers.Max(e => e.EndDate);
+        long flight = this.FlightOffers.Max(f => f.EndDate);
+
+        var values = new long[] {hotel, excursion, flight};
+
+        return values.Max();
+    }
     public double Discount { get; set; }
 
     public string Description { get; set; }
 
-    public ICollection<Offer> Offers { get; set; } = null!;
+    public ICollection<HotelOffer> HotelOffers { get; set; } = null!;
+
+    public ICollection<ExcursionOffer> ExcursionOffers { get; set; } = null!;
+
+    public ICollection<FlightOffer> FlightOffers { get; set; } = null!;
 
     public Package(string description, double discount = 0)
     {
