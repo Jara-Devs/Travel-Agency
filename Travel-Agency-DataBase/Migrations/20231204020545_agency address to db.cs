5a79398e5e213add4545b1dba73372156b436bcd
alexsierra45 <alexsierraalcala@gmail.com>
2023-12-03 21:06:22 -0500

    agency address migration

---
+++ Travel-Agency-DataBase/Migrations/20231204020545_agency address to db.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Travel_Agency_DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class agencyaddresstodb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserIdentities_Name",
+                table: "UserIdentities");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nationality",
+                table: "UserIdentities",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "UserIdentities",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityDocument",
+                table: "UserIdentities",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Agencies",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserIdentities_IdentityDocument_Nationality",
+                table: "UserIdentities",
+                columns: new[] { "IdentityDocument", "Nationality" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserIdentities_IdentityDocument_Nationality",
+                table: "UserIdentities");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Agencies");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nationality",
+                table: "UserIdentities",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "UserIdentities",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityDocument",
+                table: "UserIdentities",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserIdentities_Name",
+                table: "UserIdentities",
+                column: "Name",
+                unique: true);
+        }
+    }
+}
